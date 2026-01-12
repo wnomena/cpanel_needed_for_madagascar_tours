574dca57d4cd9e310e3dbfa2b8e3080d2c7eaf61
Nomena <jsdev.web2@gmail.com>
2026-01-12 10:54:54 +0300

    finalisation du controller pour le getter avec erreur pour le moment

--- Presenter/Getter_Method/Data_Modeliser_Before_Client.py
+++ Presenter/Getter_Method/Data_Modeliser_Before_Client.py
@@ -1,30 +1,53 @@
-from Model.Database_Model.flask_sqlalchemy import Circuit_Model, Itinerary_Model
+from Model.Database_Model.flask_sqlalchemy import Circuit_Model, Equipement_Model, Itinerary_Model,Included_task_in_Price_Model
 from Model.Getter_Method.get_all_data import Instance_of_All_Data
 import asyncio
 import pandas as pd
  
 class Modeliser_Class(Instance_of_All_Data):
-    circuit_like_hash:list[Circuit_Model]
-    itinerary_like_hash:list[Itinerary_Model]
+    __circuit_like_hash:list[Circuit_Model]
+    __itinerary_like_hash:list[Itinerary_Model]
+    __eqipement_like_hash:list[Equipement_Model]
+    __included_in_price:list[Included_task_in_Price_Model]
     def __init__(self, db):
         super().__init__(db)
 # traitement pour modeliser les données de circcuit
-    async def For_Loop_For_Circuit(self,dataframe:pd.DataFrame):
+    async def __For_Loop_For_Circuit(self,dataframe:pd.DataFrame):
         for index,rows in dataframe.iterrows():
-            await asyncio.to_thread(self.circuit_like_hash.append(Circuit_Model(id=rows["id"],title=rows["title"],subtitle=rows["subtitle"],description=rows["description"],duration=rows["duration"],difficulty=rows["difficulty"],price=rows["price"],image=rows["image"])))
+            await asyncio.to_thread(self.__circuit_like_hash.append(Circuit_Model(id=rows["id"],title=rows["title"],subtitle=rows["subtitle"],description=rows["description"],duration=rows["duration"],difficulty=rows["difficulty"],price=rows["price"],image=rows["image"])))
     async def Circuit_Modeliser(self):
         data = pd.DataFrame(self.circuit).drop_duplicates()
-        await self.For_Loop_For_Circuit(data)
-        return self.circuit_like_hash
+        await self.__For_Loop_For_Circuit(data)
+        return self.__circuit_like_hash
 
 #traitement pour les itineraires
-
-    async def For_Loop_For_Itinerary(self,dataframe:pd.DataFrame):
+    async def __For_Loop_For_Itinerary(self,dataframe:pd.DataFrame):
         for index,rows in dataframe.iterrows():
-            await asyncio.to_thread(self.circuit_like_hash.append(Itinerary_Model(id=rows["id"],place=rows["place"],order_id=rows["order_id"],circuit_id=rows["circuit_id"])))
+            await asyncio.to_thread(self.__itinerary_like_hash.append(Itinerary_Model(id=rows["id"],place=rows["place"],order_id=rows["order_id"],circuit_id=rows["circuit_id"])))
     
 
     async def Itinerary_Modeliser(self):
         data = pd.DataFrame(self.itineraire).drop_duplicates()
-        await self.For_Loop_For_Itinerary(data)
+        await self.__For_Loop_For_Itinerary(data)
+        return self.__itinerary_like_hash
 
+#traitement pour les équipements
+
+    async def __For_Loop_For_Equipement(self,dataframe:pd.DataFrame):
+        for index,rows in dataframe.iterrows():
+            await asyncio.to_thread(self.__eqipement_like_hash.append(Equipement_Model(id=rows["id"],equipment=rows["equipment"],circuit_id=rows["circuit_id"])))
+
+    async def Equipment_Modeliser(self):
+        data = pd.DataFrame(self.equipement_needed).drop_duplicates()
+        await self.__For_Loop_For_Equipement(data)
+        return self.__eqipement_like_hash
+    
+#traitement de included in price
+
+    async def __For_Loop_For_Included(self,dataframe:pd.DataFrame):
+        for index,rows in dataframe.iterrows():
+            await asyncio.to_thread(self.__included_in_price.append(Equipement_Model(id=rows["id"],equipment=rows["equipment"],circuit_id=rows["circuit_id"])))
+
+    async def Equipment_Modeliser(self):
+        data = pd.DataFrame(self.included).drop_duplicates()
+        await self.__For_Loop_For_Included(data)
+        return self.__eqipement_like_hash
--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from Model.Database_Model.flask_sqlalchemy import db
 import dotenv
+import asyncio
 import os
+
+from Presenter.Getter_Method.Data_Modeliser_Before_Client import Modeliser_Class
 
 dotenv.load_dotenv()
 app = Flask(__name__)
@@ -13,9 +16,16 @@ database_hosting = os.getenv("database_hosting")
 app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+aiomysql://{user}:{password}@{database_hosting}:3306/{database_name}"
 db.init_app(app=app)
 
+getter_from_controller = Modeliser_Class(db)
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def get_all_section():
+    return {
+        "circuit" : asyncio.run(getter_from_controller.Circuit_Modeliser),
+        "itineraire" : asyncio.run(getter_from_controller.Itinerary_Modeliser),
+        "equipement" : asyncio.run(getter_from_controller.Equipment_Modeliser),
+        "included" : asyncio.run(getter_from_controller.included)
+    }
 
 if __name__ == "__main__":
     app.run()
